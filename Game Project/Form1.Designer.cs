6fed785d7b821257112168d2ed6b0f1e239eba6d
William Beale <S23000003597@leggott.ac.uk>
2024-11-18 11:22:09 +0000

    Fixed semi-issue in autosizing not letting the user shrink the UI back down

--- Game Project/Form1.Designer.cs
+++ Game Project/Form1.Designer.cs
@@ -40,7 +40,6 @@
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.SizeChanged += new System.EventHandler(this.Form1_Rescale);
-            this.AutoSize = true;
 
         }
 
