9631bc8cfb0689a84be5bac487138693864fcd79
William Beale <S23000003597@leggott.ac.uk>
2024-12-10 10:12:55 +0000

    Removed autoscrolling in right direction within the the area for the cards on the table.

--- Game Project/Form1.cs
+++ Game Project/Form1.cs
@@ -610,7 +610,7 @@ namespace Game_Project
                 gameTableList.BorderStyle = BorderStyle.FixedSingle;
                 gameTableList.BackColor = Color.White;
                 gameTableList.AutoScroll = true;
-
+                gameTableList.Padding = new Padding(0, 0, 10, 0); // Pad to prevent horizontal scrollbar from forming
 
                 // Save Game Button
                 gameSaveGame = new Button();
